82888183a2a5e6fc6e04451015a9c3c89879d12e
Irieda Noboru <irieda@144lab.com>
2019-12-10 18:16:36 +0900

    sampleはシリアルポートモニタ

--- sample/sample.ino
+++ sample/sample.ino
@@ -1,14 +1,12 @@
-#include <SPI.h>
-#include <SPIMemory.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
 #include <bluefruit.h>
 
-/* For a list of EIR data types see:
- *    https://www.bluetooth.com/specifications/assigned-numbers/generic-access-profile
- *    Matching enum: cores/nRF5/SDK/components/softdevice/s132/headers/ble_gap.h
- */
-
-// Sample
-// 02-01-06-1A-FF-81-03-07-80-E4-B2-44-00-79-0B-00-00-80-00-8C-89-A5-46-BB-2B-1D-00-00-00-C4-00
+// BLE Service
+BLEDfu bledfu;    // OTA DFU service
+BLEDis bledis;    // device information
+BLEUart bleuart;  // uart over ble
+BLEBas blebas;    // battery
 
 #define LED 26
 #define CS 17
@@ -18,222 +16,116 @@
 #define RESET 6
 #define WP 7
 
-SPIClass SPI1(NRF_SPIM2, MISO, SCK, MOSI);
-int8_t pins[] = {SCK, MISO, MOSI, CS};
-SPIFlash flash(pins);
-
 void setup() {
-  Serial.setPins(31, 30);
   Serial.begin(115200);
-  while (!Serial) delay(10);  // for nrf52840 with native usb
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, 0);
 
-  pinMode(RESET, OUTPUT);
-  pinMode(WP, OUTPUT);
-  digitalWrite(RESET, 0);
-  digitalWrite(WP, 1);
-  delay(50);
-  digitalWrite(RESET, 1);
-  delay(5000);
+  delay(500);  // while (!Serial) delay(10);  // for nrf52840 with native usb
 
-  // flash.begin();
+  // Setup the BLE LED to be enabled on CONNECT
+  // Note: This is actually the default behaviour, but provided
+  // here in case you want to control this LED manually via PIN 19
+  Bluefruit.autoConnLed(true);
 
-  /*
-  String inputString = "This is a test String";
-  flash.writeStr(0, inputString);
-  String outputString = "";
-  if (flash.readStr(0, outputString)) {
-    Serial.print(F("Read string: "));
-    Serial.println(outputString);
-  }
-  */
+  // Config the peripheral connection with maximum bandwidth
+  // more SRAM required by SoftDevice
+  // Note: All config***() function must be called before begin()
+  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
 
-  // Initialize Bluefruit with maximum connections as Peripheral = 0, Central =
-  // 1 SRAM usage required by SoftDevice will increase dramatically with number
-  // of connections
-  Bluefruit.begin(0, 1);
+  Bluefruit.begin();
   Bluefruit.setTxPower(4);  // Check bluefruit.h for supported values
+  Bluefruit.setName("sample");
+  // Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple
+  // central connections
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
 
-  // Set the device name
-  Bluefruit.setName("Bluefruit52");
+  // To be consistent OTA DFU should be added first if it exists
+  bledfu.begin();
 
-  // Set the LED interval for blinky pattern on BLUE LED
-  Bluefruit.setConnLedInterval(250);
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
 
-  /* Start Central Scanning
-   * - Enable auto scan if disconnected
-   * - Filter out packet with a min rssi
-   * - Interval = 100 ms, window = 50 ms
-   * - Use active scan (used to retrieve the optional scan response adv packet)
-   * - Start(0) = will scan forever since no timeout is given
+  // Configure and Start BLE Uart Service
+  bleuart.begin();
+
+  // Start BLE Battery Service
+  blebas.begin();
+  blebas.write(100);
+
+  // Set up and start advertising
+  startAdv();
+}
+
+void startAdv(void) {
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleuart 128-bit uuid
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   *
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html
    */
-  Bluefruit.Scanner.setRxCallback(scan_callback);
-  Bluefruit.Scanner.restartOnDisconnect(true);
-  Bluefruit.Scanner.filterRssi(-80);
-  // Bluefruit.Scanner.filterUuid(BLEUART_UUID_SERVICE); // only invoke callback
-  // if detect bleuart service
-  Bluefruit.Scanner.setInterval(160, 80);  // in units of 0.625 ms
-  Bluefruit.Scanner.useActiveScan(true);   // Request scan response data
-  Bluefruit.Scanner.start(0);  // 0 = Don't stop scanning after n seconds
-
-  Serial.println("Scanning ...");
-}
-
-const uint8_t header[] = {0x02, 0x01, 0x06, 0x1A, 0xFF, 0x81, 0x03, 0x07,
-                          0x80, 0xE4, 0xB2, 0x44, 0x00, 0x79, 0x0B};
-
-void scan_callback(ble_gap_evt_adv_report_t* report) {
-  PRINT_LOCATION();
-  uint8_t len = 0;
-  uint8_t buffer[32];
-  memset(buffer, 0, sizeof(buffer));
-
-  // Display the timestamp and device address
-  // if (report->type.scan_response) {
-  //  return;
-  //}
-  // MAC is in little endian --> print reverse
-  // Serial.printBufferReverse(report->peer_addr.addr, 6, ':');
-  // Serial.print("\n");
-
-  if (report->data.len >= 30) {
-    if (memcmp(report->data.p_data, header, 5) == 0) {
-      // Raw buffer contents
-      Serial.printf("%14s %d bytes\n", "PAYLOAD", report->data.len);
-      if (report->data.len) {
-        Serial.printf("%15s", " ");
-        Serial.printBuffer(report->data.p_data, report->data.len, '-');
-        Serial.println();
-      }
-    }
-  }
-
-  /*
-  // RSSI value
-  Serial.printf("%14s %d dBm\n", "RSSI", report->rssi);
-
-  // Adv Type
-  Serial.printf("%14s ", "ADV TYPE");
-  if (report->type.connectable) {
-    Serial.print("Connectable ");
-  } else {
-    Serial.print("Non-connectable ");
-  }
-
-  if (report->type.directed) {
-    Serial.println("directed");
-  } else {
-    Serial.println("undirected");
-  }
-
-  // Shortened Local Name
-  if (Bluefruit.Scanner.parseReportByType(
-          report, BLE_GAP_AD_TYPE_SHORT_LOCAL_NAME, buffer,
-  sizeof(buffer))) { Serial.printf("%14s %s\n", "SHORT NAME", buffer);
-    memset(buffer, 0, sizeof(buffer));
-  }
-
-  // Complete Local Name
-  if (Bluefruit.Scanner.parseReportByType(report,
-                                          BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME,
-                                          buffer, sizeof(buffer))) {
-    Serial.printf("%14s %s\n", "COMPLETE NAME", buffer);
-    memset(buffer, 0, sizeof(buffer));
-  }
-
-  // TX Power Level
-  if (Bluefruit.Scanner.parseReportByType(
-          report, BLE_GAP_AD_TYPE_TX_POWER_LEVEL, buffer,
-  sizeof(buffer))) { Serial.printf("%14s %i\n", "TX PWR LEVEL",
-  buffer[0]); memset(buffer, 0, sizeof(buffer));
-  }
-
-  // Check for UUID16 Complete List
-  len = Bluefruit.Scanner.parseReportByType(
-      report, BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_COMPLETE, buffer,
-      sizeof(buffer));
-  if (len) {
-    printUuid16List(buffer, len);
-  }
-
-  // Check for UUID16 More Available List
-  len = Bluefruit.Scanner.parseReportByType(
-      report, BLE_GAP_AD_TYPE_16BIT_SERVICE_UUID_MORE_AVAILABLE, buffer,
-      sizeof(buffer));
-  if (len) {
-    printUuid16List(buffer, len);
-  }
-
-  // Check for UUID128 Complete List
-  len = Bluefruit.Scanner.parseReportByType(
-      report, BLE_GAP_AD_TYPE_128BIT_SERVICE_UUID_COMPLETE, buffer,
-      sizeof(buffer));
-  if (len) {
-    printUuid128List(buffer, len);
-  }
-
-  // Check for UUID128 More Available List
-  len = Bluefruit.Scanner.parseReportByType(
-      report, BLE_GAP_AD_TYPE_128BIT_SERVICE_UUID_MORE_AVAILABLE,
-  buffer, sizeof(buffer)); if (len) { printUuid128List(buffer, len);
-  }
-
-  // Check for BLE UART UUID
-  if (Bluefruit.Scanner.checkReportForUuid(report,
-  BLEUART_UUID_SERVICE)) { Serial.printf("%14s %s\n", "BLE UART", "UUID
-  Found!");
-  }
-
-  // Check for DIS UUID
-  if (Bluefruit.Scanner.checkReportForUuid(report,
-                                           UUID16_SVC_DEVICE_INFORMATION))
-  { Serial.printf("%14s %s\n", "DIS", "UUID Found!");
-  }
-
-  // Check for Manufacturer Specific Data
-  len = Bluefruit.Scanner.parseReportByType(
-      report, BLE_GAP_AD_TYPE_MANUFACTURER_SPECIFIC_DATA, buffer,
-      sizeof(buffer));
-  if (len) {
-    Serial.printf("%14s ", "MAN SPEC DATA");
-    Serial.printBuffer(buffer, len, '-');
-    Serial.println();
-    memset(buffer, 0, sizeof(buffer));
-  }
-
-  Serial.println();
-  */
-
-  // For Softdevice v6: after received a report, scanner will be
-  // paused We need to call Scanner resume() to continue scanning
-  Bluefruit.Scanner.resume();
-}
-
-void printUuid16List(uint8_t* buffer, uint8_t len) {
-  Serial.printf("%14s %s", "16-Bit UUID");
-  for (int i = 0; i < len; i += 2) {
-    uint16_t uuid16;
-    memcpy(&uuid16, buffer + i, 2);
-    Serial.printf("%04X ", uuid16);
-  }
-  Serial.println();
-}
-
-void printUuid128List(uint8_t* buffer, uint8_t len) {
-  (void)len;
-  Serial.printf("%14s %s", "128-Bit UUID");
-
-  // Print reversed order
-  for (int i = 0; i < 16; i++) {
-    const char* fm = (i == 4 || i == 6 || i == 8 || i == 10) ? "-%02X" : "%02X";
-    Serial.printf(fm, buffer[15 - i]);
-  }
-
-  Serial.println();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);  // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);    // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);  // 0 = Don't stop advertising after n seconds
 }
 
 void loop() {
-  // nothing to do
+  // Forward data from HW Serial to BLEUART
+  while (Serial.available()) {
+    // Delay to wait for enough input, since we have a limited transmission
+    // buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    bleuart.printBuffer(buf, count, '-');
+  }
+
+  // Forward from BLEUART to HW Serial
+  while (bleuart.available()) {
+    uint8_t ch;
+    ch = (uint8_t)bleuart.read();
+    Serial.write(ch);
+  }
+}
+
+// callback invoked when central connects
+void connect_callback(uint16_t conn_handle) {
+  // Get the reference to current connection
+  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  char central_name[32] = {0};
+  connection->getPeerName(central_name, sizeof(central_name));
+
+  bleuart.print("Connected to ");
+  bleuart.println(central_name);
+}
+
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle connection where this event happens
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
+  (void)conn_handle;
+  (void)reason;
 }
